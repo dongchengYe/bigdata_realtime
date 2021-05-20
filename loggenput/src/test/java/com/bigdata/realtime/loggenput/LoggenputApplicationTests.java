076f75b59a6355fd302450494cac85dbab464f53
ydc <laoye.1@outlook.com>
2021-05-20 20:03:12 +0800

    new realtime project

---
+++ loggenput/src/test/java/com/bigdata/realtime/loggenput/LoggenputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.realtime.loggenput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggenputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
