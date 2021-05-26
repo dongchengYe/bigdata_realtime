c4ce014b048871ff8cf4adc272bce62ae18613ea
ydc <laoye.1@outlook.com>
2021-05-26 21:06:44 +0800

    新建对外接口项目

---
+++ publisher/src/main/java/com/bigdata/realtime/PublisherApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherApplication.class, args);
+    }
+
+}
